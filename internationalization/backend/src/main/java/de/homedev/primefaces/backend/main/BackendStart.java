579f600613fe1f45ac4f09710d8e71b0ece35589
mikhavia <Viatcheslav.Mikhalev@vermop.com>
2018-10-25 17:26:22 +0200

    "internationalization" implemented

---
+++ internationalization/backend/src/main/java/de/homedev/primefaces/backend/main/BackendStart.java
@@ -0,0 +1,56 @@
+package de.homedev.primefaces.backend.main;
+
+import java.rmi.registry.Registry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+
+import de.homedev.primefaces.api.fassade.IPersonFassade;
+import de.homedev.primefaces.api.fassade.IUserFassade;
+import de.homedev.primefaces.backend.config.DbConfig;
+import de.homedev.primefaces.backend.util.RmiServerUtils;
+
+/**
+ * 
+ * @author Mikhalev, Viatcheslav
+ * @email slava.mikhalev@gmail.com
+ * 
+ *
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = { "de.homedev.primefaces.api", "de.homedev.primefaces.backend" })
+@Import({ DbConfig.class })
+@PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = false)
+public class BackendStart {
+	private static final Logger log = LoggerFactory.getLogger(BackendStart.class);
+
+	public static void main(String[] args) {
+		try {
+			ConfigurableApplicationContext ctx = SpringApplication.run(BackendStart.class, args);
+			int port = Integer.valueOf(ctx.getEnvironment().getRequiredProperty("app.rmi.port"));
+			log.info("rmi server port: " + port);
+			// Start RMI Server
+			Registry registry = RmiServerUtils.createRegistry(port);
+			IUserFassade userFassade = (IUserFassade) ctx.getBean(IUserFassade.SERVICE_NAME);
+			RmiServerUtils.bindUserFassade(registry, userFassade);
+			IPersonFassade personFassade = (IPersonFassade) ctx.getBean(IPersonFassade.SERVICE_NAME);
+			RmiServerUtils.bindPersonFassade(registry, personFassade);
+
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+
+	// @Bean
+	// public UserDetailsService getUserDetailsService(@Autowired
+	// ConfigurableApplicationContext ctx) {
+	// return (UserDetailsService) ctx.getBean(IUserService.SERVICE_NAME);
+	// }
+
+}
---
+++ internationalization/frontend/src/main/java/de/homedev/primefaces/frontend/bean/SimplePersonBean.java
@@ -0,0 +1,78 @@
+package de.homedev.primefaces.frontend.bean;
+
+import java.io.Serializable;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.xml.bind.JAXBException;
+
+import de.homedev.primefaces.api.fassade.IPersonFassade;
+import de.homedev.primefaces.api.model.PersonEntity;
+import de.homedev.primefaces.api.util.ApiUtil;
+import de.homedev.primefaces.frontend.util.RmiClientUtil;
+
+@ManagedBean(name = "simplePersonBean")
+@ViewScoped
+public class SimplePersonBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+    protected static final String ERROR_TO_VIEW_ID = "STAYONTHESAMEPAGE";
+    private static Logger log = Logger.getLogger(SimplePersonBean.class.getName());
+    private PersonEntity personEntity;
+
+    private Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void changeLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale(language));
+    }
+
+    public PersonEntity getPersonDto() {
+        if (personEntity == null) {
+            personEntity = new PersonEntity();
+        }
+        return personEntity;
+    }
+
+    protected boolean validate() {
+        return true;
+    }
+
+    public void toBestaetigung() throws Exception {
+        try {
+            boolean validationResult = validate();
+            if (!validationResult) {
+                return;
+            }
+            IPersonFassade fassade = RmiClientUtil.getPersonFassade(1099, "localhost");
+            personEntity = fassade.save(personEntity);
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            facesContext.getApplication().getNavigationHandler().handleNavigation(facesContext, null,
+                "/bestaetigung.jsf?faces-redirect=true");
+        } catch (Exception e) {
+            log.log(Level.SEVERE, e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    public void toPerson() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        facesContext.getApplication().getNavigationHandler().handleNavigation(facesContext, null, "/person.jsf?faces-redirect=true");
+    }
+
+    public String toPersonXML() throws JAXBException {
+        return ApiUtil.objectToXMLFile(personEntity);
+    }
+
+}
